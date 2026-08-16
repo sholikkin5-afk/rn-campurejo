261b92765d8b08054e9249f623cc71baa32f5d1d
sholikkin5-afk <sholikkin5@gmail.com>
2026-08-16 22:51:52 +0700

    Add files via upload

--- sw-admin.js
+++ sw-admin.js
@@ -1,1 +1,17 @@
-self.addEventListener('fetch', (e) => {});
+const CACHE = 'admin-v2';
+const URLS = ['/admin.html', '/manifest-admin.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
